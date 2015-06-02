223fd4e9747586e6843bff1861cd6f1a33b95b8a
Driphter <TheDriphter@gmail.com>
2015-06-01 21:59:15 -0400

    Removed unnecessary type annotations.

--- src/main/clojure/example_mod/version.clj
+++ src/main/clojure/example_mod/version.clj
@@ -1,5 +1,5 @@
 (ns example-mod.version)
 
-(def ^String mod-id "example-mod")
-(def ^String mod-name "Example Mod")
-(def ^String mod-ver "0.1.0")
+(def mod-id "example-mod")
+(def mod-name "Example Mod")
+(def mod-ver "0.1.0")
