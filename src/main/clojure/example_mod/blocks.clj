2f37d82ca2eb34d84f8b960f0463455be614030f
Driphter <TheDriphter@gmail.com>
2015-06-01 21:53:39 -0400

    Refactored a bit and added support for `defmod` in `clj-forge.mod`

--- src/main/clojure/example_mod/blocks.clj
+++ src/main/clojure/example_mod/blocks.clj
@@ -1,13 +1,14 @@
 (ns example-mod.blocks
   (:use [clojure.string :only [lower-case]]
-        [clj-forge.blocks])
+        clj-forge.blocks
+        example-mod.version)
   (:require [example-mod.tabs :as ex-tabs])
   (:import (net.minecraft.block.material Material)
            (net.minecraft.block Block)))
 
 (def lambda-block
   (create-block :name           "lambda-block"
-                :mod-id         "example-mod"
+                :mod-id         mod-id
                 :material       Material/iron
                 :creative-tab   ex-tabs/example-tab
                 :step-sound     Block/soundTypeStone
